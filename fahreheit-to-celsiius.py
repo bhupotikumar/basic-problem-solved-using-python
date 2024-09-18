b7c58a2e8701d8782a82fab7c00265016560f996
bhupotikumar <bhupotikumar123@gmail.com>
2024-09-18 23:20:16 +0600

    Fahrenheit to Celsius

---
+++ fahreheit-to-celsiius.py
@@ -0,0 +1,4 @@
+# Input value in Fahrenheit
+fahrenheit  = int(input("Enter temperature in Fahrenheit: "))
+celsius = int((fahrenheit - 32) * 5 / 9)
+print("The Temperature is : ", celsius, "Degree Celsius")
